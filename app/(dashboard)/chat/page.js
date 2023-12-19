117572b8a5a28820b942bc0cb801e175c8e1fb82
Fahim <fhasan5@wgu.edu>
2023-12-19 12:17:56 -0600

    Initial Push

---
+++ app/(dashboard)/chat/page.js
@@ -0,0 +1,19 @@
+import {
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+  dehydrate,
+} from '@tanstack/react-query';
+
+import Chat from '../../../components/Chat';
+
+const ChatPage = async () => {
+  const queryClient = new QueryClient();
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Chat />
+    </HydrationBoundary>
+  );
+};
+
+export default ChatPage;
